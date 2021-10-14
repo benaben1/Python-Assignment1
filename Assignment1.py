1c2e2682769b19fc9256d825b625bb34a346f591
benaben1 <nabwirebenaa17@gmail.com>
2021-10-14 08:20:25 +0300

    python Assignment1

---
+++ Assignment1.py
@@ -0,0 +1,16 @@
+
+# Assigning multiple values to multiple variables
+
+name = "Bena"
+Age = str(20)
+School = "WITI"
+Teacher = "Miriam"
+
+print("My name is " +name + ", i am " + Age + "years old, i study from " + School + ", and my teacher is " + Teacher)
+
+# Assigning one value to multiple variables
+
+monday = tuesday = wednesday = "Present"
+print(monday)
+print(tuesday)
+print(wednesday)
